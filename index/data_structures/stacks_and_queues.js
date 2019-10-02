09f09c110ea5aeca5c593a3e1af05bc92ac699c9
Brittany Hartmire <bkhartmire@gmail.com>
2019-10-02 21:07:52 +0900

    SetOfStacks

--- index/data_structures/stacks_and_queues.js
+++ index/data_structures/stacks_and_queues.js
@@ -1,15 +1,54 @@
-// 3.1 Describe houw you could use a single array to implement three stacks.
+// 3.1 Describe how you could use a single array to implement three stacks.
+
+//array of 3 nested arrays representing stacks
 
 // 3.2 How would you design a stack which, in addition to push and pop, also has a function min which returns the minimum element?
 // Push, pop, and min should all operate in O(1) time.
 
-// 3.3 Imagine a (literal) stack fo plates. If the stack gets too high, it might topple.
+//besides using Math.min(stack)??
+
+// 3.3 Imagine a (literal) stack of plates. If the stack gets too high, it might topple.
 // Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold.
 // Implement a data structure SetOfStacks that mimics this.
 // SetOfStacks should be composed of several stacks and should create a new stack once the previous once exceeds capacity.
 // SetOfStacks.push() and SetOfStacks.pop() should begave identically to a single stack (that is, pop() should return the same values as it would if there just a single stack).
 // FOLLOW UP
 // Implement a function popAt(int index) which performs a pop operation on a specific sub-stack
+
+class SetOfStacks {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.stacks = [[]];
+  }
+
+  push(item) {
+    if (this.stacks[this.stacks.length - 1].length < this.capacity) {
+      this.stacks[this.stacks.length - 1].push(item);
+    } else {
+      this.stacks.push([item]);
+    }
+  }
+
+  pop() {
+    const result = this.stacks[this.stacks.length - 1].pop();
+    if (this.stacks[this.stacks.length - 1].length === 0) this.stacks.pop();
+    return result;
+  }
+
+  popAt(index) {
+    const result = this.stacks[index].pop();
+    if (this.stacks[index].length === 0) this.stacks.splice(index, 1);
+    return result;
+  }
+}
+// const newSet = new SetOfStacks(1);
+// newSet.push(1);
+// newSet.push(1);
+// newSet.push(1);
+// console.log(newSet.stacks);
+// console.log(newSet.pop());
+// console.log(newSet.pop());
+// console.log(newSet.stacks);
 
 // 3.4 In the class problem of the Towers of Hanoi, you have 3 towers and N disks of different sizes which can slide onto any tower.
 // The puzzle starts with disks sorted in ascending order of size from top to bottom (i.e., each disk sits on top of an even larger one).
