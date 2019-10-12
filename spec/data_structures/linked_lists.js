854734a6e23f6d93433e19d9a8faa618305f43be
Brittany Hartmire <bkhartmire@gmail.com>
2019-10-11 23:54:01 -0700

    export with commonJS

--- spec/data_structures/linked_lists.js
+++ spec/data_structures/linked_lists.js
@@ -9,10 +9,33 @@ const {
   getLoopedNode,
   isPalindrome
 } = require("../../index/data_structures/linked_lists");
-
+let linkedList;
 describe("linked list algorithms", () => {
+  beforeEach(() => {
+    console.log("inside before each");
+    linkedList = new Node(1);
+  });
+
   describe("removeDups", () => {
-    it("should", () => {});
+    it("should remove duplicates from an unsorted linked list", () => {
+      //Dup Linked List
+      // 1 -> 2 -> 2 -> 3 -> 1 -> 4 -> 5
+      const two = new Node(2);
+      linkedList.next = two;
+      const twoDup = new Node(2);
+      two.next = twoDup;
+      const three = new Node(3);
+      twoDup.next = three;
+      const oneDup = new Node(1);
+      three.next = oneDup;
+      const four = new Node(4);
+      oneDup.next = four;
+      const five = new Node(5);
+      four.next = five;
+      removeDups(linkedList);
+      expect(two.next).to.deep.equal(three);
+      expect(three.next).to.deep.equal(four);
+    });
   });
   describe("findKthToLastNode", () => {
     it("should", () => {});
