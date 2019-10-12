11bf32283d36f6bb8ca381509a17ab9069ded5eb
Brittany Hartmire <bkhartmire@gmail.com>
2019-10-11 23:18:39 -0700

    describe blocks

--- spec/data_structures/linked_lists.js
+++ spec/data_structures/linked_lists.js
@@ -11,5 +11,25 @@ const {
 } = require("../../index/data_structures/linked_lists");
 
 describe("linked list algorithms", () => {
-  it("should", () => {});
+  describe("removeDups", () => {
+    it("should", () => {});
+  });
+  describe("findKthToLastNode", () => {
+    it("should", () => {});
+  });
+  describe("deleteNode", () => {
+    it("should", () => {});
+  });
+  describe("partition", () => {
+    it("should", () => {});
+  });
+  describe("sumLinkedLists", () => {
+    it("should", () => {});
+  });
+  describe("getLoopedNode", () => {
+    it("should", () => {});
+  });
+  describe("isPalindrome", () => {
+    it("should", () => {});
+  });
 });
