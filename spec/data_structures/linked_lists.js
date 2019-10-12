dee709b995738b18ea7d60348b9c5d3643fc9133
Brittany Hartmire <bkhartmire@gmail.com>
2019-10-11 23:16:25 -0700

    set up specs

---
+++ spec/data_structures/linked_lists.js
@@ -0,0 +1,15 @@
+const { expect } = require("chai");
+const {
+  Node,
+  removeDups,
+  findKthToLastNode,
+  deleteNode,
+  partition,
+  sumLinkedLists,
+  getLoopedNode,
+  isPalindrome
+} = require("../../index/data_structures/linked_lists");
+
+describe("linked list algorithms", () => {
+  it("should", () => {});
+});
