d4b32422228cc4276992bf3257936453bd0bd1d4
Brittany Hartmire <bkhartmire@gmail.com>
2019-10-23 23:25:03 -0700

    pushing creates new stack spec

--- spec/data_structures/stacks_and_queues.js
+++ spec/data_structures/stacks_and_queues.js
@@ -8,4 +8,13 @@ describe("SetOfStacks", () => {
     const newSet = new SetOfStacks(5);
     expect(newSet.capacity).to.equal(5);
   });
+  it("should create a new stack if pushing item exceeds capacity", () => {
+    const newSet = new SetOfStacks(2);
+    newSet.push("a");
+    expect(newSet.stacks).to.deep.equal([["a"]]);
+    newSet.push("b");
+    expect(newSet.stacks).to.deep.equal([["a", "b"]]);
+    newSet.push("c");
+    expect(newSet.stacks).to.deep.equal([["a", "b"], ["c"]]);
+  });
 });
